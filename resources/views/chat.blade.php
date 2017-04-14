c41d16aab2b7da1059719277f5566ee4a3faea25
jimmy8646 <yujiechueh@gmail.com>
2017-04-15 00:04:26 +0800

    init

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <script>
+        // rename myToken as you like
+        window.myToken =  <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+        </script>
+        <title>阿傑的聊天室</title>
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+        <div id="app">
+            <h1>聊天室</h1>
+            <example></example>
+        </div>
+        <script src=js/app.js charset="utf-8"></script>
+    </body>
+</html>
